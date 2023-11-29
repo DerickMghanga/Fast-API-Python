bb7eb20490f6412afbdce239d29081a6be49370a
DerickMghanga <derickmghanga3329@gmail.com>
2023-11-29 10:02:53 +0300

    pydantic

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 # Create an Instance for fastAPI
 app = FastAPI()
 
+
+
+# POST request schema(pydantic model) expected data from client
+class Post(BaseModel):
+    title: str
+    content: str
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my first Python API"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.title)
+    print(new_post.content)
+    return {"data": "new post"}
+
+
+# Schema example >> title: str, content: str, category: str, published: Bool
